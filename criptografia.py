ced2318feeb74614cbe3018dc53283670a4aa836
Leonardo Canedo <64497153+LeonardoCanedo@users.noreply.github.com>
2022-10-06 15:21:28 -0300

    Fazendo o upload da primeira versão
    Este é o primeiro commit do projeto, deixei o código bem comentado para vocês terem uma ideia.
    (O ideal é não deixar o código todo comentado por que polui o código, algumas coisas ok mas expliquem o que vocês fizerem pelos commits.)
    
    Até agora temos um progama que recebe uma string, converte ela para números e adiciona a cada número +3.
    
    A fazeres:
    - Converter números já somados a base binária
    - Identificar se o usuário quer encripitar uma string ou decriptar um código
    - Desenvolver algoritimo de decriptação

---
+++ criptografia.py
@@ -0,0 +1,38 @@
+#caixa de texto que recebera o texto
+phrase = input("Insira o texto que deseja encriptar/decriptar:\n")
+#separando a frase em letras, colocando em um array
+letters = list(phrase)
+#criando um array para os numeros
+numbers = []
+
+#definindo função para transformar letras em números, não podemos deixar que isso rode automaticamente pq na hora de decodificar ele faria isso e daria errado.
+def lettersToNumbers():
+  #essa função passa por todos os itens do array letters e transforma eles em números, se o número for negativo, ele arredonda, por exemplo espaço retorna -64, então transformamos ele em 64. O comando ord ja abrange também as letras maiusculas
+  for element in letters:
+    convertedNumber = ord(element) - 96
+    if (convertedNumber < 0):
+      convertedNumber = convertedNumber * -1
+
+    numbers.append(convertedNumber)
+#executando a função lettersToNumbers
+lettersToNumbers()
+print(letters)
+print(numbers)
+#definindo e executando outra função que pega os numeros convertidos e adiciona +3 a cada um
+def addToNumbers():
+  addedNumbers= []
+  for number in numbers:
+    newNumber = number + 3
+    addedNumbers.append(newNumber)
+  print(addedNumbers)
+
+addToNumbers()
+
+#Exemplo de execução do progama
+
+
+#Insira o texto que deseja encriptar/decriptar:
+#leo
+#['l', 'e', 'o']
+#[12, 5, 15]
+#[15, 8, 18]
