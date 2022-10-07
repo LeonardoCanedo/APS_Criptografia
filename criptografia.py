913569a09e677e951cd82c451e16a6ab2b7775e0
Leonardo Canedo <64497153+LeonardoCanedo@users.noreply.github.com>
2022-10-07 11:51:53 -0300

    Atualização e organização
    Agora o progama retorna um array com os números binários já convertidos e invertidos. Ele funciona ao chamar a última função criada então cuidado com a linha que chama as funções.
    
    Também refatorei parte do código, estava poluido tudo em uma única função então separei em duas, temos uma função para converter para binário e outra para reverter cada número.
    
    O progama executa a última função criada (no caso a de reverter ) passando os argumentos corretos (as outras funções que retornam os arrays necessários para a manipulação)
    
    O progama agora retorna um array com arrays dentro, os de dentro são os que a gente precisa converte-los novamente para string. Além disso:
    - Identificar se o progama vai codificar ou decodificar
    -Algoritmo(função(ões)) para descriptografar
    
    Tentem seguir o mesmo modelo de funções, assim o código fica mais limpo e enxuto.
    
    Amo vcs <3

--- criptografia.py
+++ criptografia.py
@@ -1,38 +1,68 @@
-#caixa de texto que recebera o texto
+# caixa de texto que recebera o texto
 phrase = input("Insira o texto que deseja encriptar/decriptar:\n")
-#separando a frase em letras, colocando em um array
+# separando a frase em letras, colocando em um array
 letters = list(phrase)
-#criando um array para os numeros
+# criando um array para os numeros
 numbers = []
 
-#definindo função para transformar letras em números, não podemos deixar que isso rode automaticamente pq na hora de decodificar ele faria isso e daria errado.
+# definindo função para transformar letras em números, não podemos deixar que isso rode automaticamente pq na hora de decodificar ele faria isso e daria errado.
+
+
 def lettersToNumbers():
-  #essa função passa por todos os itens do array letters e transforma eles em números, se o número for negativo, ele arredonda, por exemplo espaço retorna -64, então transformamos ele em 64. O comando ord ja abrange também as letras maiusculas
-  for element in letters:
-    convertedNumber = ord(element) - 96
-    if (convertedNumber < 0):
-      convertedNumber = convertedNumber * -1
+    # essa função passa por todos os itens do array letters e transforma eles em números, se o número for negativo, ele arredonda, por exemplo espaço retorna -64, então transformamos ele em 64. O comando ord ja abrange também as letras maiusculas
+    for element in letters:
+        convertedNumber = ord(element) - 96
+        if (convertedNumber < 0):
+            convertedNumber = convertedNumber * -1
 
-    numbers.append(convertedNumber)
-#executando a função lettersToNumbers
+        numbers.append(convertedNumber)
+
+
+# executando a função lettersToNumbers
 lettersToNumbers()
-print(letters)
-print(numbers)
-#definindo e executando outra função que pega os numeros convertidos e adiciona +3 a cada um
+# print(letters)
+# print(numbers)
+
+# definindo função que pega os numeros convertidos e adiciona +3 a cada um
 def addToNumbers():
-  addedNumbers= []
-  for number in numbers:
-    newNumber = number + 3
-    addedNumbers.append(newNumber)
-  print(addedNumbers)
-
-addToNumbers()
-
-#Exemplo de execução do progama
+    addedNumbers = []
+    for number in numbers:
+        newNumber = number + 3
+        addedNumbers.append(newNumber)
+    # print(addedNumbers)
+    return addedNumbers
 
 
-#Insira o texto que deseja encriptar/decriptar:
-#leo
+def addedNumbersToBinary(array):
+    binaryArray = []
+    for addedNumber in array:
+        newBinary = format(addedNumber, "b")
+        binaryArray.append(newBinary)
+
+    return binaryArray
+    #binaryArray retorna o array com os numeros agora convertidos em binário
+
+def reverseBinaryNumbers(binaryArrayCreated):
+    reversedBinaryArray = []
+    for i in binaryArrayCreated:
+        stringList = list(i)
+        stringList.reverse()
+        reversedBinaryArray.append(stringList)
+    print(reversedBinaryArray)
+
+
+
+reverseBinaryNumbers(addedNumbersToBinary(addToNumbers()))
+
+
+# def binaryInversion(listToArrays):
+
+
+# Exemplo de execução do progama
+
+
+# Insira o texto que deseja encriptar/decriptar:
+# leo
 #['l', 'e', 'o']
 #[12, 5, 15]
 #[15, 8, 18]
