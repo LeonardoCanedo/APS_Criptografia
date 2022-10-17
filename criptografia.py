61960fb3701618fd16bc870369d1fe5c37d8c140
Leonardo Canedo <64497153+LeonardoCanedo@users.noreply.github.com>
2022-10-17 13:40:32 -0300

    Codificação e decodificação completa
    Agora o progama funciona tanto para codificar quanto para decodificar as mensagens escritas pelo usuário.
    Resolvemos os problemas para identificação de plavras/letras com splits e um código específico para sabermos onde as palavras/letras tem um final.
    Coloquei breaks no loop while para que quando o progama terminar a codificação/decodificação ele encerre.
    Refatorei o código para que ele fique mais organizado, agora ele está todo separado em funções e o que executa cada função na ordem correta com os argumentos corretos são as linhas de comando que se encontram em cada setor de idenficação, ou seja, se for para decodificar ele só executa as funções de decodificação e vise-versa. 
    
    Temos um produto entregável :)

--- criptografia.py
+++ criptografia.py
@@ -1,78 +1,87 @@
-# caixa de texto que recebera o texto
+
 Options1 = input("Digite 1 para Encriptar ou 2 para Descriptar: ")
-#enquanto a pessoa nao inserir 1 ou 2 mostra mensagem de erro e volta para o inicio do código
+
+numbers = []
+def lettersToNumbers():
+  for element in letters:
+    convertedNumber = ord(element) - 96
+    numbers.append(convertedNumber)
+
+def addToNumbers():
+  addedNumbers = []
+  for number in numbers:
+      newNumber = number + 3
+      addedNumbers.append(newNumber)
+  return addedNumbers
+
+def addedNumbersToBinary(array):
+    binaryArray = []
+    for addedNumber in array:
+        newBinary = format(addedNumber, "b")
+        binaryArray.append(newBinary)            
+    return binaryArray
+
+def reverseBinaryNumbers(binaryArrayCreated):
+    reversedBinaryArray = []
+    for i in binaryArrayCreated:
+      stringList = list(i)
+      stringList.reverse()
+      stringList.append("100000001")
+      reversedBinaryArray.append(stringList)
+    return reversedBinaryArray
+
+def arrayToString(array):
+    encripted = []
+    for i in array:
+      string = "".join(str(x) for x in i)
+      encripted.append(string)
+
+    encripted = "".join(str(x) for x in encripted)
+    print(encripted)
+    return encripted
+
+def binarytowords():
+    listapalavras = ' '.join(phrase2[::-1] for phrase2 in phrase2.split("1100001" and "100000001"))            
+    reversedList = listapalavras.split(" ")
+    reversedList.pop()           
+    return reversedList        
+
+def binaryToDecimal(array):      
+    decimal_rep = []
+    for i in array:              
+      decimal = int(i, 2)
+      decimal_rep.append(decimal)            
+    return decimal_rep
+
+def decimalminus3(array):
+    numSub = []
+    for i in array:
+      numSub.append(i-3)            
+    return numSub
+
+def numToLetter(array):
+    newLetters = []
+    for i in array:
+      teste = i+96
+      convertedNumber = chr(teste)
+      newLetters.append(convertedNumber)                                        
+    return newLetters
+        
+def joiningLetters(array):
+    teste = ''.join(array)
+    print(teste)
+
 while (Options1 != "1" or Options1 != "2"):    
     if Options1 == "1":
         phrase = input("Insira o texto que deseja encriptar:\n")
-        # separando a frase em letras, colocando em um array
-
-
         letters = list(phrase)
-        # criando um array para os numeros
-        numbers = []
-
-        # definindo função para transformar letras em números, não podemos deixar que isso rode automaticamente pq na hora de decodificar ele faria isso e daria errado.
-        def lettersToNumbers():
-            # essa função passa por todos os itens do array letters e transforma eles em números, se o número for negativo, ele arredonda, por exemplo espaço retorna -64, então transformamos ele em 64. O comando ord ja abrange também as letras maiusculas
-            for element in letters:
-                convertedNumber = ord(element) - 96
-                if (convertedNumber < 0):
-                    convertedNumber = convertedNumber * -1
-
-                numbers.append(convertedNumber)
-
-        # executando a função lettersToNumbers
         lettersToNumbers()
-
-        # definindo função que pega os numeros convertidos e adiciona +3 a cada um
-        def addToNumbers():
-            addedNumbers = []
-            for number in numbers:
-                newNumber = number + 3
-                addedNumbers.append(newNumber)
-            # print(addedNumbers)
-            return addedNumbers
-
-        def addedNumbersToBinary(array):
-            binaryArray = []
-            for addedNumber in array:
-                newBinary = format(addedNumber, "b")
-                binaryArray.append(newBinary)
-
-            return binaryArray
-            #binaryArray retorna o array com os numeros agora convertidos em binário
-
-        def reverseBinaryNumbers(binaryArrayCreated):
-            reversedBinaryArray = []
-            for i in binaryArrayCreated:
-                stringList = list(i)
-                stringList.reverse()
-                reversedBinaryArray.append(stringList)
-            return reversedBinaryArray
-
-        def arrayToString(array):
-            encripted = []
-            for i in array:
-                string = "".join(str(x) for x in i)
-                encripted.append(string)
-
-            encripted = "".join(str(x) for x in encripted)
-            print(encripted)
-            return encripted
-
-
         arrayToString(reverseBinaryNumbers(addedNumbersToBinary(addToNumbers())))
+        break
 
     elif Options1 == "2":
         phrase2 = input("Insira o texto que deseja Descriptar:\n")
-        #essa funçao faz com que toda a frase encripitada seja separada no número "1100001" e cria uma lista separada com os resultados restantes.
-        def binarytowords():
-            listapalavras = phrase2.split("1100001")            
-            print(listapalavras)
-            return listapalavras
-            
-        binarytowords()
-          
+        joiningLetters(numToLetter(decimalminus3(binaryToDecimal(binarytowords()))))
+        break
     else:
         Options1 = input("[ERRO] Valor digitado Invalido!\n Digite 1 para Encriptar ou 2 para Descriptar: ")
-
